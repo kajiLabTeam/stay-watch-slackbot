5eda94d0b25c8313925127774e700ede63ea77e7
Bayashin <k21099kk@aitech.ac.jp>
2024-07-01 18:24:02 +0900

    add:staywatchのconfigを追加

--- src/conf/config.go
+++ src/conf/config.go
@@ -3,16 +3,29 @@ package conf
 import "github.com/spf13/viper"
 
 var s *viper.Viper
+var sw *viper.Viper
 
 func init() {
 	s = viper.New()
 	s.SetConfigType("yaml")
 	s.SetConfigName("slackbot")
 	s.AddConfigPath("conf/environments/")
+
+	sw = viper.New()
+	sw.SetConfigType("yaml")
+	sw.SetConfigName("stay_watch")
+	sw.AddConfigPath("conf/environments/")
 }
 
 func GetSlackConfig() *viper.Viper {
 	if err := s.ReadInConfig(); err != nil {
+		return nil
+	}
+	return s
+}
+
+func GetStayWatchConfig() *viper.Viper {
+	if err := sw.ReadInConfig(); err != nil {
 		return nil
 	}
 	return s
