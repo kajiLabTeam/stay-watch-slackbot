64c454a3e8733879d13fc223c7ac0f342e5a6502
Bayashin <k21099kk@aitech.ac.jp>
2025-04-12 18:34:46 +0900

    feat

--- src/router/router.go
+++ src/router/router.go
@@ -14,33 +14,10 @@ func Router() {
 	gin.DefaultWriter = io.MultiWriter(f)
 
 	r := gin.Default()
-	r.SetTrustedProxies([]string{"127.0.0.1"})
-
-	// r.Use(cors.New(cors.Config{
-	// アクセスを許可したいアクセス元
-	// 	AllowOrigins: []string{
-	// 		"https://",
-	// 		"http://localhost:3000",
-	// 	},
-	// 	// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
-	// 	AllowMethods: []string{
-	// 		"POST",
-	// 	},
-	// 	// 許可したいHTTPリクエストヘッダ
-	// 	AllowHeaders: []string{
-	// 		"Access-Control-Allow-Credentials",
-	// 		"Access-Control-Allow-Headers",
-	// 		"Content-Type",
-	// 		"Content-Length",
-	// 		"Accept-Encoding",
-	// 		"Accept",
-	// 		"Authorization",
-	// 	},
-	// 	// cookieなどの情報を必要とするかどうか
-	// 	AllowCredentials: true,
-	// 	// preflightリクエストの結果をキャッシュする時間
-	// 	MaxAge: 24 * time.Hour,
-	// }))
+	r.SetTrustedProxies([]string{
+		"103.21.244.0/22",
+		"2400:cb00:2048:1::/64",
+	})
 
 	r.POST("/slack/events", controller.PostSlackEvents)
 	r.POST("/slack/interaction", controller.PostSlackInteraction)
