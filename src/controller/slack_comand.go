c8dbf44d24599c95567aa044e012e4e34a71ae76
Bayashin <k21099kk@aitech.ac.jp>
2025-04-14 21:00:23 +0900

    fix

--- src/controller/slack_comand.go
+++ src/controller/slack_comand.go
@@ -96,37 +96,29 @@ func PostRegisterCorrespondCommand(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "bad request"})
 		return
 	}
-
-	tags, err := service.GetTags()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
-		return
-	}
-
-	var options []*slack.OptionBlockObject
-	for _, tag := range tags {
-		option := slack.OptionBlockObject{
-			Text:  slack.NewTextBlockObject("plain_text", tag.Name, false, false),
-			Value: fmt.Sprintf("%d", tag.ID),
-		}
-		options = append(options, &option)
-	}
-
 	modalRequest := slack.ModalViewRequest{
-		Type:       slack.VTModal,
-		CallbackID: "select_tags",
-		Title:      slack.NewTextBlockObject("plain_text", "ユーザ選択", false, false),
-		Close:      slack.NewTextBlockObject("plain_text", "閉じる", false, false),
-		Submit:     slack.NewTextBlockObject("plain_text", "決定", false, false),
+		Type:       slack.ViewType("modal"),
+		Title:      slack.NewTextBlockObject("plain_text", "登録フォーム", false, false),
+		Submit:     slack.NewTextBlockObject("plain_text", "送信", false, false),
 		PrivateMetadata: s.ResponseURL,
+		Close:      slack.NewTextBlockObject("plain_text", "閉じる", false, false),
+		CallbackID: "register_tag",
 		Blocks: slack.Blocks{
 			BlockSet: []slack.Block{
-				slack.InputBlock{
-					Type:    slack.MBTInput,
-					BlockID: "tag_select_block",
-					Label:   slack.NewTextBlockObject("plain_text", "話題を選択してください", false, false),
-					Element: slack.NewCheckboxGroupsBlockElement("tag_checkbox", options...),
-				},
+				// 名前
+				slack.NewInputBlock(
+					"name_block",
+					slack.NewTextBlockObject("plain_text", "話題を入力してください", false, false),
+					slack.NewTextBlockObject("plain_text", "名前", false, false),
+					slack.NewPlainTextInputBlockElement(slack.NewTextBlockObject("plain_text", "例：スマブラ、Android", false, false), "name_input"),
+				),
+				// 人数
+				slack.NewInputBlock(
+					"number_block",
+					slack.NewTextBlockObject("plain_text", "最低限必要な人数を入力してください", false, false),
+					slack.NewTextBlockObject("plain_text", "人数", false, false),
+					slack.NewPlainTextInputBlockElement(slack.NewTextBlockObject("plain_text", "例：2", false, false), "number_input"),
+				),
 			},
 		},
 	}
@@ -137,4 +129,51 @@ func PostRegisterCorrespondCommand(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"response_type": "in_channel", "text": "モーダルを開きました。"})
+	// s, err := slack.SlashCommandParse(c.Request)
+	// if err != nil {
+	// 	log.Printf("Error parsing slash command: %v", err)
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+	// 	return
+	// }
+
+	// tags, err := service.GetTags()
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+	// 	return
+	// }
+
+	// var options []*slack.OptionBlockObject
+	// for _, tag := range tags {
+	// 	option := slack.OptionBlockObject{
+	// 		Text:  slack.NewTextBlockObject("plain_text", tag.Name, false, false),
+	// 		Value: fmt.Sprintf("%d", tag.ID),
+	// 	}
+	// 	options = append(options, &option)
+	// }
+
+	// modalRequest := slack.ModalViewRequest{
+	// 	Type:       slack.VTModal,
+	// 	CallbackID: "select_tags",
+	// 	Title:      slack.NewTextBlockObject("plain_text", "ユーザ選択", false, false),
+	// 	Close:      slack.NewTextBlockObject("plain_text", "閉じる", false, false),
+	// 	Submit:     slack.NewTextBlockObject("plain_text", "決定", false, false),
+	// 	PrivateMetadata: s.ResponseURL,
+	// 	Blocks: slack.Blocks{
+	// 		BlockSet: []slack.Block{
+	// 			slack.InputBlock{
+	// 				Type:    slack.MBTInput,
+	// 				BlockID: "tag_select_block",
+	// 				Label:   slack.NewTextBlockObject("plain_text", "話題を選択してください", false, false),
+	// 				Element: slack.NewCheckboxGroupsBlockElement("tag_checkbox", options...),
+	// 			},
+	// 		},
+	// 	},
+	// }
+	// _, err = api.OpenView(s.TriggerID, modalRequest)
+	// if err != nil {
+	// 	log.Printf("Error opening view: %v", err)
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+	// 	return
+	// }
+	// c.JSON(http.StatusOK, gin.H{"response_type": "in_channel", "text": "モーダルを開きました。"})
 }
